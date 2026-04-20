e69d51a8d7273e1db0c2053384be386ed10bec36
xngsoo <hanxngsoo@gmail.com>
2026-04-20 15:10:07 +0900

    프로젝트 생성

---
+++ Dama/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dama
+//
+//  Created by SEUNGSOO HAN on 4/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dama/DamaApp.swift
@@ -0,0 +1,17 @@
+//
+//  DamaApp.swift
+//  Dama
+//
+//  Created by SEUNGSOO HAN on 4/20/26.
+//
+
+import SwiftUI
+
+@main
+struct DamaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
